eb3d9939d724530c32efcaa5e3807b51f5499e80
Daniel Kriesten <krid78@users.noreply.github.com>
2022-12-06 11:56:40 +0100

    Add solution part 2 of day 6

---
+++ 06/communication_part2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+""" Advent of Code 2022/12/05
+https://adventofcode.com/2022/day/5
+"""
+
+
+def get_data(filename: str) -> list:
+    """
+    Return file contents as list
+    """
+    with open(filename, "r") as in_file:
+        content = [line.strip() for line in in_file]
+
+    return content
+
+
+def main():
+    # the_data = get_data("data_test.txt")
+    the_data = get_data("data.txt")
+
+    for line in the_data:
+        # print(line)
+        for idx in range(len(line) - 13):
+            # print(f"Trying: line[idx:idx+4]")
+            if len(set(line[idx : idx + 14])) == 14:
+                print(f"Found marker from {idx=} to {(idx + 14)=}")
+                break
+
+
+if __name__ == "__main__":
+    main()
