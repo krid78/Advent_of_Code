467764073c71892aee389b250d734fd0929aa993
Daniel Kriesten <krid78@users.noreply.github.com>
2022-12-10 17:08:49 +0100

    Fix solutions for day 9

--- 09/rope_part2.py
+++ 09/rope_part2.py
@@ -23,12 +23,7 @@ def main():
     the_data = get_data("data.txt")
 
     total = 0
-    directions = {
-        "R": (1, 0),
-        "L": (-1, 0),
-        "U": (0, 1),
-        "D": (0, -1),
-    }
+    directions = {"R": (1, 0), "L": (-1, 0), "U": (0, 1), "D": (0, -1)}
     total = set()
     knots = [
         [0, 0],
@@ -48,45 +43,22 @@ def main():
         direction, steps = move.split()
         for _ in range(int(steps)):
             # move Head
-            dirx, diry = directions[direction]
-            knots[0][0] += dirx
-            knots[0][1] += diry
+            dx, dy = directions[direction]
+            knots[0][0] += dx
+            knots[0][1] += dy
             for idx in range(1, len(knots)):
                 dx = knots[idx - 1][0] - knots[idx][0]
                 dy = knots[idx - 1][1] - knots[idx][1]
-                """
-                if move in x and delta in x == 2 and delta in y == 1; move in x and y
-                if move in x and delta in x == 2; move in x
-                if move in x and delta in y == 2; move in y
-                if move in y and delta in x == 1 and delta in y == 2; move in x and y
-                if move in y and delta in y == 2; move in y
-                if move in y and delta in x == 2; move in x
-                """
-                if (abs(dirx) == 1 and abs(dx) == 2 and abs(dy) == 1) or (
-                    abs(diry) == 1 and abs(dy) == 2 and abs(dx) == 1
-                ):
-                    knots[idx][0] = knots[idx - 1][0] - dirx
-                    knots[idx][1] = knots[idx - 1][1] - diry
-                elif abs(dirx) == 1 and abs(dx) == 2 and abs(dy) == 0:
-                    if dx != 0:
-                        knots[idx][0] += dx // abs(dx)
-                elif abs(diry) == 1 and abs(dy) == 2 and abs(dx) == 0:
-                    if dy != 0:
-                        knots[idx][1] += dy // abs(dy)
-                elif abs(dirx) == 1 and abs(dy) == 2:
-                    if dx != 0:
-                        knots[idx][0] += dx // abs(dx)
-                    if dy != 0:
-                        knots[idx][1] += dy // abs(dy)
-                elif abs(diry) == 1 and abs(dx) == 2:
-                    if dx != 0:
-                        knots[idx][0] += dx // abs(dx)
-                    if dy != 0:
-                        knots[idx][1] += dy // abs(dy)
-                else:
-                    break
+                if abs(dx) >= 2 and dy == 0:
+                    knots[idx][0] += dx // abs(dx)
+                elif abs(dy) >= 2 and dx == 0:
+                    knots[idx][1] += dy // abs(dy)
+                elif abs(dx) + abs(dy) >= 3:
+                    knots[idx][0] += dx // abs(dx)
+                    knots[idx][1] += dy // abs(dy)
 
             total.add((knots[9][0], knots[9][1]))
+
             print(
                 f"Head: {knots[0][0]}, {knots[0][1]} and Tail: {knots[9][0]},"
                 f"{knots[9][1]}; Uniques: {len(total)}"
