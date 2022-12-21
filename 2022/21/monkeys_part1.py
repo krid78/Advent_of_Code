78c14d5557c83614ef02769518caec21c3bb354b
Daniel Kriesten <krid78@users.noreply.github.com>
2022-12-21 20:50:29 +0100

    Add solution part 1 for day 21

--- 2022/21/monkeys_part1.py
+++ 2022/21/monkeys_part1.py
@@ -13,36 +13,40 @@ def get_data(filename: str) -> list:
 
     return content
 
+
 def solve(_monkeys: dict, name: str):
     """rekursive solve puzzle"""
 
+    if type(_monkeys[name]) == int:
+        return _monkeys[name]
 
-# def main():
-"""code if module is called directly"""
-# the_data = get_data("data_test1.txt")
-the_data = get_data("data.txt")
+    operand1 = solve(_monkeys, _monkeys[name][0])
+    operand2 = solve(_monkeys, _monkeys[name][2])
+    res = eval(f"{operand1} {_monkeys[name][1]} {operand2}")
+    return res
 
-monkeys = {}
 
-for data in the_data:
-    try:
+def main():
+    """code if module is called directly"""
+    # the_data = get_data("data_test1.txt")
+    the_data = get_data("data.txt")
+
+    monkeys = {}
+
+    for data in the_data:
         monkey, knowledge = tuple(data.split(":"))
-        monkeys[monkey.strip()] = knowledge.strip()
-    except ValueError:
-        print(f"ValueError\n  {data=}")
-        knowledge = None
+        knowledge = knowledge.strip().split()
 
-    if type(knowledge) == str and len(knowledge.strip().split()) > 3:
-        print(f"{knowledge.split()}")
-        break
+        if len(knowledge) > 1:
+            monkeys[monkey.strip()] = knowledge
+        else:
+            monkeys[monkey.strip()] = int(knowledge[0])
 
-print(f"root: {monkeys['root']}")
+    solution = solve(monkeys, "root")
 
-    
-
-    # return best
+    return solution
 
 
-# if __name__ == "__main__":
-#     solution = main()
-#     print(f"{solution=}")
+if __name__ == "__main__":
+    solution = main()
+    print(f"{solution=}")
