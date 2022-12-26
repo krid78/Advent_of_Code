dd93d2e9b40649fcd3f00426903f0ef4f3ac7c86
Daniel Kriesten <krid78@users.noreply.github.com>
2022-12-26 14:19:59 +0100

    Add solution for part 1 of day 25

--- 2022/25/SNAFU_part1.py
+++ 2022/25/SNAFU_part1.py
@@ -4,9 +4,9 @@ https://adventofcode.com/2022/day/25
 
 Decimal | SNAFU | Solution
 ---------------------------------
-  1     |     = | -2 * 5^0
-  1     |     - | -1 * 5^0
-  1     |     0 |  0 * 5^0
+ -2     |     = | -2 * 5^0
+ -1     |     - | -1 * 5^0
+  0     |     0 |  0 * 5^0
   1     |     1 |  1 * 5^0
   2     |     2 |  2 * 5^0
   3     |    1= |  1 * 5^1 - 2 * 5^0
@@ -21,7 +21,43 @@ Decimal | SNAFU | Solution
  20     |   1-0 |  1 * 5^2 - 2 * 5^1 - 0 * 5^0
 """
 
-import math
+snafu_table = {
+    -2: "=",
+    -1: "-",
+    0: "0",
+    1: "1",
+    2: "2",
+    3: "1=",
+    4: "1-",
+    5: "10",
+    6: "11",
+    7: "12",
+    8: "2=",
+    9: "2-",
+    10: "20",
+    11: "21",
+    12: "22",
+    13: "1==",
+    14: "1=-",
+    15: "1=0",
+    16: "1=1",
+    17: "1=2",
+    18: "1-=",
+    19: "1--",
+    20: "1-0",
+    21: "1-1",
+    22: "1-2",
+    23: "10=",
+    24: "10-",
+    25: "100",
+    26: "101",
+    27: "102",
+    28: "11=",
+    29: "11-",
+    30: "110",
+    31: "111",
+    32: "112",
+}
 
 
 def get_data(filename: str) -> list:
@@ -29,7 +65,7 @@ def get_data(filename: str) -> list:
     Return file contents as list
     """
     with open(filename, "r") as in_file:
-        content = [row.rstrip() for row in in_file]
+        content = [row.strip() for row in in_file]
 
     return content
 
@@ -47,84 +83,63 @@ def snafu2dez(snafu: str) -> int:
         # print(f"{idx=}, {token=}, {trans=}, {quot=}, {exp=} => {val=}")
 
     # print(f"{val=}")
-
     return int(val)
 
 
 def dez2snafu(dez_in: int) -> str:
-    """translate dez to SNAFU"""
-    translate = ["=", "-", "0", "1", "2"]
+    """translate dez to SNAFU
+    unfortunately, we do not find the exp using log"""
+    # translate = ["=", "-", "0", "1", "2"]
+    translate = {3: "=", 4: "-", 0: "0", 1: "1", 2: "2"}
     dez = dez_in
     snafu = ""
-    exp = int(1 + math.log(dez_in, 5))
-    while exp >= 0:
-        rest = dez % 5**exp
-        idx = (rest + 2) % len(translate)
-        dez = dez - rest * 5**exp
-        snafu = translate[idx] + snafu
-        exp -= 1
+    snafu_l = []
+
+    if dez == -2 or dez == -1:
+        return translate[dez % 5]
+
+    if dez == 0:
+        snafu_l.insert(0, dez % 5)
+
+    while dez > 0:
+        rest = dez % 5
+        snafu_l.insert(0, rest)
+
+        dez //= 5
+
+    # print(f"{dez_in=} => {snafu_l=}")
+
+    if snafu_l[0] > 2:
+        snafu_l.insert(0, 0)
+    for i in range(len(snafu_l)):
+        if snafu_l[i] >= 3:
+            snafu_l[i - 1] += 1
+            snafu_l[i - 1] %= 5
+
+    for s in snafu_l:
+        snafu += translate[s]
+
+    # print(f"{dez_in=} => {snafu_l=} => {snafu=}")
 
     return snafu
 
 
-def snafu2dezX(snafu: str) -> int:
-    """translate SNAFU to dez"""
+def main():
+    """code if module is called directly"""
+    # the_data = get_data("data_test1.txt")
+    # the_data = get_data("data_test2.txt")
+    the_data = get_data("data.txt")
 
-    translate = ["=", "-", "0", "1", "2"]
-    length = len(snafu)
-    val = 0
-    assert length > 0
-    for idx, token in enumerate(snafu):
-        quot = translate.index(token)
-        exp = length - idx - 1
-        val += int(quot * 5**exp)
-        # print(f"{idx=}, {token=}, {trans=}, {quot=}, {exp=} => {val=}")
+    dez = 0
 
-    # print(f"{val=}")
+    for data in the_data:
+        dez += snafu2dez(data)
 
-    return int(val)
+    solution = dez2snafu(dez)
+
+    return solution
 
 
-def dez2snafuX(dez_in: int) -> str:
-    """translate dez to SNAFU"""
-    translate = ["=", "-", "0", "1", "2"]
-    dez = dez_in
-    snafu = ""
-
-    while dez_in > 0:
-        rest = dez_in % 5
-        dez_in //= 5
-        idx = rest
-        snafu = translate[idx] + snafu
-
-    return snafu
-
-
-# def main():
-"""code if module is called directly"""
-the_data = get_data("data_test1.txt")
-# the_data = get_data("data_test2.txt")
-# the_data = get_data("data.txt")
-
-dez = 0
-dezX = 0
-
-for data in the_data:
-    # print(f"{data} = ", end="")
-    dez += snafu2dez(data)
-    # print(f"{dez}")
-    dezX += snafu2dezX(data)
-
-solution = dez2snafu(dez)
-solutionX = dez2snafuX(dez)
-
-print(f"{dez} => {solution=}")
-print(f"{dezX} => {solutionX=}")
-
-# return solution
-# return solutionX
-
-
-# if __name__ == "__main__":
-#     solution = main()
-#     print(f"{solution=}")
+if __name__ == "__main__":
+    solution = main()
+    print(f"{solution=}")
