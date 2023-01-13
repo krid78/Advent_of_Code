16b813d5fc4b397852d9047c867d78f6f81ecdce
Daniel Kriesten <krid78@users.noreply.github.com>
2023-01-13 21:58:39 +0100

    Remove the debugging in part 1

--- 2022/16/valves_part1.py
+++ 2022/16/valves_part1.py
@@ -47,35 +47,32 @@ def get_valves(data):
 
 def nq_sort(status):
     """https://www.youtube.com/watch?v=3-VJC_KRUZ0&t=330s"""
-    _, _, t_left, r_pressure, _ = status
+    _, _, t_left, r_pressure = status
     return 10 * t_left + 100 * r_pressure
 
 
 def main():
     """code if module is called directly"""
-    the_data = get_data("data_test1.txt")
-    # the_data = get_data("data.txt")
+    # the_data = get_data("data_test1.txt")
+    the_data = get_data("data.txt")
 
     valves = get_valves(the_data)
 
-    logfile = open("valves_part1.log", "w")
     # store best values
     best = set()
 
-    # current valve, opened valves, time left, pressure released, way
-    next_nodes = [("AA", (), 30, 0, ())]
+    # current valve, opened valves, time left, pressure released, cur. pressure
+    next_nodes = [("AA", (), 30, 0)]
 
     while next_nodes:
-        print(f"{len(next_nodes)}")
         nodes = deque(sorted(next_nodes, key=nq_sort, reverse=True)[:10000])
         next_nodes.clear()
 
         while nodes:
-            valve, opened, t_left, r_pressure, way = nodes.popleft()
+            valve, opened, t_left, r_pressure = nodes.popleft()
 
             if t_left <= 0:
                 best.add(r_pressure)
-                logfile.write(f"{r_pressure}, {way=}, {opened=}\n")
                 continue
 
             if valve not in opened and valves[valve]["fl"] != 0:
@@ -83,7 +80,6 @@ def main():
                 t_left -= 1
                 r_pressure += t_left * valves[valve]["fl"]
 
-            way = tuple(way + (valve,))
             for subsequent in valves[valve]["conn"]:
                 next_nodes.append(
                     (
@@ -91,11 +87,8 @@ def main():
                         opened,
                         t_left - 1,
                         r_pressure,
-                        way,
                     )
                 )
-
-    logfile.close()
 
     return max(best)
 
