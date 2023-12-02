7ce8ea1603b022dafd8074987d817e93fbe549b3
Daniel Kriesten <krid78@users.noreply.github.com>
2023-12-02 13:46:18 +0100

    Solution and data for day1 part1

---
+++ 2023/01/day01_part1.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""Solve Advent of Code 2023/12/01
+https://adventofcode.com/2021/day/1
+"""
+
+import string
+
+
+def get_data(filename: str) -> list:
+    """Return file contents as list"""
+    with open(filename, "r") as in_file:
+        content = [row.rstrip() for row in in_file]
+
+    return content
+
+
+def main():
+    """Solve day 01"""
+    solution = 0
+    # the_data = get_data("day01_test1.txt")
+    the_data = get_data("day01_data.txt")
+
+    for line in the_data:
+        for letter in line:
+            if letter in string.digits:
+                left = letter
+                break
+        print(f"{line}: {left}")
+
+        for letter in line[::-1]:
+            if letter in string.digits:
+                right = letter
+                break
+        print(f"{line}: {right}")
+
+        print(f"{line}: {left}{right}")
+        solution += int(f"{left}{right}")
+
+    return solution
+
+
+if __name__ == "__main__":
+    solution = main()
+    print(f"{solution=}")
