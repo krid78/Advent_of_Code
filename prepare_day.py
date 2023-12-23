aa3bd1376c6b44bce02c7a88a5972e171229ab64
Daniel Kriesten <krid78@users.noreply.github.com>
2023-12-23 19:49:47 +0100

    Update the templating solution

--- prepare_day.py
+++ prepare_day.py
@@ -12,34 +12,72 @@ import os
 import argparse
 from datetime import datetime
 
+__TEMPLATE__ = '''"""Solve Advent of Code {year}, day {day}
+
+https://adventofcode.com/{year}/day/{day}
+"""
+
+
+def get_data(filename: str) -> list:
+    """Return file contents as list"""
+    with open(filename, "r") as in_file:
+        content = [row.rstrip() for row in in_file]
+
+    return content
+
+
+def solve():
+    """Solve the puzzle"""
+    solution1 = 0
+    solution2 = 0
+
+    # the_data = get_data("day{day:02}.data")
+    the_data = get_data("day{day:02}.test")
+
+    return solution1, solution2
+
+
+if __name__ == "__main__":
+    solution1, solution2 = solve()
+'''
 
 def prepare_day(year, day):
     # Create directory if it doesn't exist
     directory_name = str(year)
     os.makedirs(directory_name, exist_ok=True)
+    print(f"Directory '{directory_name}' and files", end="")
 
     # Create file with the specified day number
-    file_name = f"day{day}.py"
+    file_name = f"day{day:02}.py"
+    file_path = os.path.join(directory_name, file_name)
+    with open(file_path, 'w') as file:
+        file.write(__TEMPLATE__.format(**{"year":year, "day":day}))
+        file.write('    print(f"{solution1=} | {solution2=}"\n')
+
+    print(f" '{file_name}'", end="")
+
+    file_name = f"day{day:02}.data"
     file_path = os.path.join(directory_name, file_name)
     with open(file_path, 'w') as file:
         pass  # Empty file
 
-    file_name = f"day{day}.data"
+    print(f", '{file_name}'", end="")
+
+    file_name = f"day{day:02}.test"
     file_path = os.path.join(directory_name, file_name)
     with open(file_path, 'w') as file:
         pass  # Empty file
 
-    file_name = f"day{day}.test"
-    file_path = os.path.join(directory_name, file_name)
-    with open(file_path, 'w') as file:
-        pass  # Empty file
+    print(f" and '{file_name}'", end="")
 
-    print(f"Directory '{directory_name}' and file '{file_name}' created successfully.")
+    print(f" created successfully.")
 
 def main():
     parser = argparse.ArgumentParser(description='Prepare day directory and file.')
-    parser.add_argument('--year', type=int, help='Four-digit year (default: current year)')
-    parser.add_argument('--day', type=int, help='Day number (default: current day)')
+    parser.add_argument('-y', '--year', type=int, help='Four-digit year (default: current year)')
+    parser.add_argument('-d', '--day', type=int, help='Day number (default: current day)')
+    args = parser.parse_args()
+
     args = parser.parse_args()
 
     # Use current year if not provided
