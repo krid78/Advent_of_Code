3cca9bfc285ff6f1ed09cab51828de9a544e32c8
Daniel Kriesten <krid78@users.noreply.github.com>
2024-12-02 16:49:09 +0100

    [Py] fix day 2

--- 2024/python/day02.py
+++ 2024/python/day02.py
@@ -15,22 +15,18 @@ def get_data(filename: str) -> list:
 def check_report(report):
     """check the given report"""
     res = False
-    print(f"{report=}", end="")
     if 0 < (int(report[0]) - int(report[1])) < 4:
         safe = {1, 2, 3}
     elif -4 < (int(report[0]) - int(report[1])) < 0:
         safe = {-1, -2, -3}
     else:
-        print(" has no safe set")
         return res, 0
 
     for idx in range(1, len(report) - 1):
         check = int(report[idx]) - int(report[idx + 1])
         if check not in safe:
-            print(f" is not safe ({report[idx]=}, {report[idx+1]=}, {check=}, {safe=})")
             break
     else:
-        print(" is safe")
         res = True
 
     return res, idx
@@ -41,28 +37,11 @@ def solve1(the_data):
     solution = 0
     for line in the_data:
         report = line.split()
-        res, _ = check_report(report)
+
+        res, idx = check_report(report)
+
         if res:
             solution += 1
-
-        # print(f"{report=}", end="")
-        # if 0 < (int(report[0]) - int(report[1])) < 4:
-        #    safe = {1, 2, 3}
-        # elif -4 < (int(report[0]) - int(report[1])) < 0:
-        #    safe = {-1, -2, -3}
-        # else:
-        #    print(" has no safe set")
-        #    continue
-        # for idx in range(1, len(report) - 1):
-        #    check = int(report[idx]) - int(report[idx + 1])
-        #    if check not in safe:
-        #        print(
-        #            f" is not safe ({report[idx]=}, {report[idx+1]=}, {check=}, {safe=})"
-        #        )
-        #        break
-        # else:
-        #    print(" is safe")
-        #    solution += 1
 
     return solution
 
@@ -75,26 +54,21 @@ def solve2(the_data):
     # drop self, check()
     # drop next, check()
     for line in the_data:
-        print("========")
         report = line.split()
 
         res, idx = check_report(report)
 
         if not res:
-            print("report=", end="")
-            print(report[:idx] + report[idx + 1 :], end="")
-            print(" --> retry")
             res, _ = check_report(report[:idx] + report[idx + 1 :])
 
         if not res:
-            print("report=", end="")
-            print(report[: idx + 1] + report[idx + 2 :], end="")
-            print(" --> retry")
             res, _ = check_report(report[: idx + 1] + report[idx + 2 :])
+
+        if not res and idx < 2:
+            res, _ = check_report(report[1:])
 
         if res:
             solution += 1
-            print(f"{solution=}")
 
     return solution
 
@@ -111,7 +85,7 @@ def solve():
     #     "/home/dkrieste/Dokumente/Develop/advent-of-code/2024/data/day02.test"
     # )
 
-    # solution1 = solve1(the_data)
+    solution1 = solve1(the_data)
     solution2 = solve2(the_data)
 
     return solution1, solution2
