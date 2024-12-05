543c6a362a5c53dba3f2852f514a770f820b91d7
Daniel Kriesten <krid78@users.noreply.github.com>
2024-12-05 18:59:09 +0100

    [Py] Code optimization

--- 2024/python/day05.py
+++ 2024/python/day05.py
@@ -4,62 +4,90 @@ https://adventofcode.com/2024/day/5
 """
 
 
-def get_data(filename: str) -> list:
-    """Return file contents as list"""
+def get_data(filename: str) -> list[str]:
+    """Return file contents as a list of strings."""
     with open(filename, "r") as in_file:
-        content = [row.rstrip() for row in in_file]
-
-    return content
+        return [row.rstrip() for row in in_file]
 
 
-def solve():
-    """Solve the puzzle"""
+def build_page_map(data: list[str]) -> dict[int, list[int]]:
+    """
+    Build the page map from the input data.
+
+    Args:
+        data: List of strings in the format 'pre|post'.
+
+    Returns:
+        Dictionary mapping 'pre' to a list of 'post' values.
+    """
+    page_map = {}
+    for line in data:
+        pre, post = map(int, line.split("|"))
+        page_map.setdefault(pre, []).append(post)
+    return page_map
+
+
+def process_update_list(
+    update_list: list[int], page_map: dict[int, list[int]]
+) -> tuple[bool, int]:
+    """
+    Process an update list to resolve conflicts based on the page map.
+
+    Args:
+        update_list: The list of numbers to process.
+        page_map: The dictionary of page mappings.
+
+    Returns:
+        A tuple containing:
+        - A boolean indicating if the list is correct.
+        - The middle value of the processed list.
+    """
+    correct = True
+    idx = 0
+
+    while idx < len(update_list):
+        current = update_list[idx]
+        left = set(update_list[:idx])
+        right = set(page_map.get(current, []))
+
+        if left & right:
+            # Conflict detected
+            correct = False
+            intersection = left & right
+            earliest_position = min(update_list.index(x) for x in intersection)
+            update_list.pop(idx)
+            update_list.insert(earliest_position, current)
+            idx = earliest_position
+        else:
+            idx += 1
+
+    middle_value = update_list[len(update_list) // 2]
+    return correct, middle_value
+
+
+def solve() -> tuple[int, int]:
+    """Solve the puzzle."""
     solution1 = 0
     solution2 = 0
 
+    # Load data
     the_data = get_data("2024/data/day05.data")
     # the_data = get_data("2024/data/day05.test")
 
+    # Split data into page map and updates
     upd_start = the_data.index("")
+    page_map = build_page_map(the_data[:upd_start])
+    update_lists = [
+        list(map(int, line.split(","))) for line in the_data[upd_start + 1 :]
+    ]
 
-    page_map = {}
-    for idx in range(upd_start):
-        pre, post = map(int, the_data[idx].split("|"))
-
-        if pre not in page_map:
-            page_map[pre] = []
-        page_map[pre].append(post)
-
-    for line in the_data[upd_start + 1 :]:
-        l = [int(x) for x in line.split(",")]
-        print(f"{l=}")
-        correct = True
-        li = 0
-        while li < len(l):
-            lv = l[li]
-            left = set(l[:li])
-            right = set(page_map.get(lv, []))
-
-            intersection = left & right
-            if left & right:
-                correct = False
-                # print(f"{lv=}, {intersection=}")
-                # print(f"{l=}")
-                i = len(left)
-                for x in intersection:
-                    i = min(i, l.index(x))
-                l.pop(li)
-                l.insert(i, lv)
-                li = i
-                # print(f"{l=}")
-            else:
-                li += 1
-
-        print(f"{l=}; {l[len(l) // 2]}")
+    # Process each update list
+    for update_list in update_lists:
+        correct, middle_value = process_update_list(update_list, page_map)
         if correct:
-            solution1 += l[len(l) // 2]
+            solution1 += middle_value
         else:
-            solution2 += l[len(l) // 2]
+            solution2 += middle_value
 
     return solution1, solution2
 
