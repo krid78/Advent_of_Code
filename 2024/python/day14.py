4a41c8c9b430cf6ca0f3367f22ae1eb4dc537309
Daniel Kriesten <krid78@users.noreply.github.com>
2024-12-14 14:07:24 +0100

    [Py] What is the a picture of a Christmas tree?

--- 2024/python/day14.py
+++ 2024/python/day14.py
@@ -4,6 +4,7 @@ https://adventofcode.com/2024/day/14
 """
 
 import time
+from collections import deque
 
 
 def get_data(filename: str) -> list[dict[str, tuple[int, int]]]:
@@ -50,8 +51,8 @@ def solve_part1(the_data, steps, max_row, max_col):
 
     for bot in the_data:
         # calculate the position of the bot after steps
-        r = (bot["p"][0] + bot["v"][0] * steps) % max_row
-        c = (bot["p"][1] + bot["v"][1] * steps) % max_col
+        r = (bot["p"][1] + bot["v"][1] * steps) % max_row
+        c = (bot["p"][0] + bot["v"][0] * steps) % max_col
         bot_final_pos.append((r, c))
         if 0 <= r < (max_row // 2) and 0 <= c < (max_col // 2):
             # top-left-quadrant
@@ -74,16 +75,60 @@ def solve_part1(the_data, steps, max_row, max_col):
     return quadrants[0] * quadrants[1] * quadrants[2] * quadrants[3]
 
 
+def solve_part2(the_data, steps, max_row, max_col):
+    """find shape of christmas tree
+     *
+    ***
+
+    Args:
+        the_data (_type_): _description_
+        steps (_type_): _description_
+        max_row (_type_): _description_
+        max_col (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    bot_pos = []
+    # do a while, until all bots have the starting position (again)
+    for step in range(steps):
+        bot_pos.clear()
+        bots_per_row = [0 for _ in range(max_row)]
+        for bot in the_data:
+            # calculate the position of the bot after steps
+            r = (bot["p"][1] + bot["v"][1] * steps) % max_row
+            c = (bot["p"][0] + bot["v"][0] * steps) % max_col
+            bots_per_row[r] += 1
+            bot_pos.append((r, c))
+
+        print(f"Max bots in a row: {max(bots_per_row)}")
+
+        while 1 in bots_per_row:
+            idx = bots_per_row.index(1)
+            depth = 1
+            expected = 3
+            while bots_per_row[idx + depth] == expected:
+                depth += 1
+                expected += 2
+            print(f"Seconds: {step+1}, row: {idx}, depth: {expected}")
+            # draw_bot_map(bot_pos, max_row, max_col)
+            bots_per_row.remove(1)
+
+    return step
+
+
 def solve():
     """Solve the puzzle."""
     solution1 = 0
     solution2 = 0
 
-    # the_data = get_data("2024/data/day14.data")
+    the_data = get_data("2024/data/day14.data")
     # solution1 = solve_part1(the_data, 100, 103, 101)
+    solution2 = solve_part2(the_data, 10000000, 103, 101)
 
-    the_data = get_data("2024/data/day14.test")
-    solution1 = solve_part1(the_data, 100, 7, 11)
+    # the_data = get_data("2024/data/day14.test")
+    # solution1 = solve_part1(the_data, 100, 7, 11)
+    # solution2 = solve_part2(the_data, 100, 7, 11)
 
     return solution1, solution2
 
