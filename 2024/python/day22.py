6387cf18f54d6dc1b262b98ef6c5bf84af1cbee3
Daniel Kriesten <krid78@users.noreply.github.com>
2024-12-22 11:08:24 +0100

    [Py] Try to solve day 22, part 2
    Small bug remaining, now its time for christmas market.

--- 2024/python/day22.py
+++ 2024/python/day22.py
@@ -28,27 +28,53 @@ def solve_intro1(secret: int = 123) -> None:
     """Reproduce the introduction"""
     s0 = secret
     sequence = []
+    prices = [s0 % 10]
+    changes = []
 
     for _ in range(10):
         s1 = ((s0 * 64) ^ s0) % 16777216
         s2 = ((s1 // 32) ^ s1) % 16777216
         s3 = ((s2 * 2048) ^ s2) % 16777216
         sequence.append(s3)
+        prices.append(s3 % 10)
+        changes.append(prices[-1] - prices[-2])
         s0 = s3
 
     print(sequence)
+    print(prices)
+    print(changes)
 
 
 def solve_intro2(secret: int = 123) -> None:
     """Reproduce the introduction"""
-    s0 = secret
-    sequence = []
 
-    for _ in range(10):
+    watcher = {}
+
+    # fill the data structures
+    d0 = d1 = d2 = d3 = 10
+    last_price = secret % 10
+    for _ in range(3):
         secret = next_secret(secret)
-        sequence.append(secret)
+        current_price = secret % 10
+        d0 = d1
+        d1 = d2
+        d2 = d3
+        d3 = current_price - last_price
+        last_price = current_price
 
-    print(sequence)
+    for _ in range(3, 10):
+        secret = next_secret(secret)
+        current_price = secret % 10
+        d0 = d1
+        d1 = d2
+        d2 = d3
+        d3 = current_price - last_price
+        watcher.setdefault((d0, d1, d2, d3), []).append(current_price)
+        last_price = current_price
+
+        # print(watcher)
+    for k, v in watcher.items():
+        print(f"{k=}, {v=}")
 
 
 def solve_part1(the_data: list[int]) -> int:
@@ -69,33 +95,65 @@ def solve_part1(the_data: list[int]) -> int:
 
 def solve_part2(the_data: list[int]) -> int:
     """Solve the puzzle."""
-    solution = 0
 
-    return solution
+    # sequence: [prices]
+    watcher = {}
+
+    for s0 in the_data:
+        # fill the data structures
+        d0 = d1 = d2 = d3 = 10
+        last_price = s0 % 10
+
+        for _ in range(3):
+            s0 = next_secret(s0)
+            current_price = s0 % 10
+            d0 = d1
+            d1 = d2
+            d2 = d3
+            d3 = current_price - last_price
+            last_price = current_price
+
+        for _ in range(3, 10):
+            s0 = next_secret(s0)
+            current_price = s0 % 10
+            d0 = d1
+            d1 = d2
+            d2 = d3
+            d3 = current_price - last_price
+            watcher.setdefault((d0, d1, d2, d3), []).append(current_price)
+            last_price = current_price
+
+    for k, v in watcher.items():
+        print(f"{k=}, {v=}")
+
+    print(f"{watcher[(-2, 1, -1, 3)]}")
+
+    return 0
 
 
 if __name__ == "__main__":
 
-    time_start = time.perf_counter()
-    solve_intro1(123)
-    print(f"Intro solved in {time.perf_counter()-time_start:.5f} Sec.")
+    # time_start = time.perf_counter()
+    # solve_intro1(123)
+    # print(f"Intro solved in {time.perf_counter()-time_start:.5f} Sec.")
 
     time_start = time.perf_counter()
     solve_intro2(123)
     print(f"Intro solved in {time.perf_counter()-time_start:.5f} Sec.")
 
-    the_data = get_data("2024/data/day22.data")
-    # the_data = get_data("2024/data/day22.test")
+    # the_data = get_data("2024/data/day22.data")
 
     # solve part 1
-    time_start = time.perf_counter()
-    solution1 = solve_part1(the_data)
-    print(f"Part 1 ({solution1}) solved in {time.perf_counter()-time_start:.5f} Sec.")
+    # time_start = time.perf_counter()
+    # solution1 = solve_part1([1,10,100,2024])
+    # solution1 = solve_part1(the_data)
+    # print(f"Part 1 ({solution1}) solved in {time.perf_counter()-time_start:.5f} Sec.")
 
     # solve part 2
     time_start = time.perf_counter()
-    solution2 = solve_part2(the_data)
+    solution2 = solve_part2([1, 2, 3, 2024])
+    # solution2 = solve_part2(the_data)
     print(f"Part 2 ({solution2}) solved in {time.perf_counter()-time_start:.5f} Sec.")
 
     # finally
-    print(f"{solution1=} | {solution2=}")
+    # print(f"{solution1=} | {solution2=}")
